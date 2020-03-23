1866fb02187688f2befeef77d51262179107b3e2
qubearfarmer <nbaolong89@gmail.com>
2020-03-23 15:07:15 -0400

    update script

--- Coherent_state.py
+++ Coherent_state.py
@@ -15,20 +15,21 @@ rho_coherent = coherent(10, 1)
 # plt.ylim([0,0.2])
 # plt.xlim([0,21])
 
-# W = wigner(rho_coherent, xvec, xvec)
-# cmap = wigner_cmap(W)
-# X, Y = np.meshgrid(xvec, xvec)
-# plt.contourf(X, Y, W, 50, cmap=cmap)
-# plt.tick_params(labelsize = 14.0)
-# path = 'C:\\Users\\nguyen89\Google Drive\Research\Illustration\Thesis\Chapter 2\coherent4.pdf'
-# plt.savefig(path, dpi=300)
-
-
-Q = qfunc(rho_coherent,xvec,xvec)
-qplt = plt.contourf(xvec, xvec, np.real(Q), 100, cmap = 'GnBu')
-plt.tick_params(labelsize = 14.0)
-path = 'C:\\Users\\nguyen89\Google Drive\Research\Illustration\Thesis\Chapter 2\coherent4.pdf'
+plt.figure(figsize = [6,6])
+W = wigner(rho_coherent, xvec, xvec)
+cmap = wigner_cmap(W)
+X, Y = np.meshgrid(xvec, xvec)
+plt.contourf(X, Y, W, 50, cmap=cmap)
+plt.tick_params(labelsize = 15.0)
+path = 'C:\\Users\\nguyen89\Google Drive\Research\Illustration\Thesis\Chapter 2\coherent3.pdf'
 plt.savefig(path, dpi=300)
+
+
+# Q = qfunc(rho_coherent,xvec,xvec)
+# qplt = plt.contourf(xvec, xvec, np.real(Q), 100, cmap = 'GnBu')
+# plt.tick_params(labelsize = 15.0)
+# path = 'C:\\Users\\nguyen89\Google Drive\Research\Illustration\Thesis\Chapter 2\coherent3.pdf'
+# plt.savefig(path, dpi=300)
 
 #thermal state
 # rho_thermal = thermal_dm(N, 2)
